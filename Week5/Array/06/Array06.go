bd40bc48aa57c54b72c42c86b4822eea7865963e
eak0546 <57831268+eak0546@users.noreply.github.com>
2019-12-14 09:17:01 +0700

    Create Array06.go

---
+++ Week5/Array/06/Array06.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := [5]int{0, 1, 2, 3, 4}
+	fmt.Println(number[1])
+	number[1] = 10
+	fmt.Println(number[1])
+	length := len(number)
+	fmt.Println("Length = ", length)
+}
