993114127a544bf62707b0f53f2145f70cef85b5
eak0546 <57831268+eak0546@users.noreply.github.com>
2019-12-29 16:11:19 +0700

    Create 04.go

---
+++ Week7/action/04/04.go
@@ -0,0 +1,12 @@
+
+func main() {
+	float max;
+	number = a[-1,-5,-6,-99];
+	  max = number[0];
+	  for int i = 0; i < len(number); i++ {
+	  if number[i] > max {
+		max = number[i];
+		}
+	  }
+	  Println(max);
+  }
