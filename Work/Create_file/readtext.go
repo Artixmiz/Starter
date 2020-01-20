2948da522e274d74b27e9f3080f8f86a80fc15b3
eak0546 <57831268+eak0546@users.noreply.github.com>
2020-01-20 07:16:04 +0700

    Create readtext.go

---
+++ Work/Create_file/readtext.go
@@ -0,0 +1,33 @@
+package Create_file
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+)
+
+func readtxt() []os.FileInfo {
+	files, err := ioutil.ReadDir("./TXT/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return files
+}
+
+func openreadtext() {
+	files := readtxt()
+	for _, file := range files {
+		content, err := ioutil.ReadFile(fmt.Sprint("TXT/", file))
+		if err != nil {
+			log.Fatal(err)
+		}
+		re := regexp.MustCompile(`[Atiwan]\w+/g`)
+		cresultName, _ := regexp.MatchString(content)
+		retur
+	}
+
+	//fmt.Printf("File contents: %s", content)
+}
